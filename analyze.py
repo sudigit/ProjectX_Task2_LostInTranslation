d00f4eb5b2a66b407e54c49c7d6e9a808dde3263
sudigit <sudikshakadam2020@gmail.com>
2025-06-20 23:22:54 +0530

    Add analyze.py

---
+++ analyze.py
@@ -0,0 +1,57 @@
+import os
+
+#function for syntax error
+def getrisk(filepath):
+    count = 0
+    forb = False
+    risk = ""
+
+    with open(filepath, 'r', encoding='utf-8') as file:
+        for line in file:
+
+            #line length
+            if len(line) > 80:
+                count += 1
+
+            #remove comment
+            x = line.split("#")[0]
+
+            #check forbidden keywords
+            if "print(" in x:
+                forb = True
+                count += 1
+            if "eval(" in x:
+                forb = True
+                count += 1
+            if "exec(" in x:
+                forb = True
+                count += 1
+
+            #check odd no of double quotes
+            if x.count('"') % 2 != 0:
+                count += 1
+
+            #check check odd no of single quotes
+            if x.count("'") % 2 != 0:
+                count += 1
+
+    # risk
+    if count > 5 or forb:
+        risk = "HIGH"
+    elif 0 < count <= 5 and not forb:
+        risk = "LOW"
+    else:
+        risk = "CLEAN"
+
+    print(f"{filepath}: {risk}")
+
+
+#get directory and the src folder
+dir = os.path.dirname(__file__)
+folder = os.path.join(dir, 'src')
+
+#go through files in folder
+for filename in os.listdir(folder):
+    file = os.path.join(folder, filename)
+    getrisk(file)
+        
